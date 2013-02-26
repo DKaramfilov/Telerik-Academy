c5ee96db86e445eeaedc861dfffde0dfc9362aa0
DKaramfilov <mpc@gbg.bg>
2013-02-26 21:28:24 +0200

    Add Tasks 3, 4 and 5

---
+++ C#/OOP/Linq_Lambda_ExtensionMethods/Task3Students/MainClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3Students //task 3,  4, and 5 at single project
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Student[] students = { new Student("Johnny","English",50), new Student("Bruce","Willis",20), 
+                                     new Student("Brad","Pit",25), new Student("Quentin"," Tarantino",24),
+                                 new Student("Sylvester","Stallone",19), new Student("Jason" ,"Statham",12),
+                                 new Student("Sylvester","Andreev",19)};
+
+            Console.WriteLine("Testing Task 3 ...................");
+            var names = from s in students
+                        where s.FName.CompareTo(s.LName) < 0
+                        select s;
+
+            foreach (Student name in names)
+            {
+                Console.WriteLine(name);   
+            }
+            Console.WriteLine();
+            Console.WriteLine("Testing Task 4 ...................");
+            var age = from s in students 
+                      where s.Age >= 18 && s.Age <= 24 
+                      select new{s1= s.FName, s2=s.LName} ;//made like this for exaple purpose
+
+            foreach (var name in age)
+            {
+                Console.WriteLine(name.s1 +" " +name.s2);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Testing Task 5 ...................");
+            Console.WriteLine("\nPrintig with lambda\n");
+            var lambdaSorted = students.OrderByDescending(x => x.FName).ThenByDescending(x => x.LName);
+            foreach (var name in lambdaSorted)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("\nPrinting wit LINQ\n");
+            var sortedStudents = from s in students  orderby s.FName descending, s.LName descending select s;
+            foreach (Student name in sortedStudents)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
