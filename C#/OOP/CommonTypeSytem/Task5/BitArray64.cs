f10a0223fbe2de3b17a003ae65cc8a4f7399875f
Dimitar Karamfilov <mpc@gbg.bg>
2013-03-24 10:44:16 +0200

    Final Common type system homework

--- C#/OOP/CommonTypeSytem/Task5/BitArray64.cs
+++ C#/OOP/CommonTypeSytem/Task5/BitArray64.cs
@@ -1,38 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task5
 {
     class BitArray64 : IEnumerable<int>
     {
         private ulong bits;
-
+        //constructor
+        public BitArray64(ulong num)
+        {
+            this.bits = num;
+        }
+        //enumerator
         public IEnumerator<int> GetEnumerator()
         {
-            throw new NotImplementedException();
+            int[] bitsArray = this.ConvertBitsToArray();
+            for (int i = 63; i >= 0; i--)
+            {
+                yield return bitsArray[i];
+            }
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
         }
+        //overrided methods
         public override bool Equals(object obj)
         {
-            return base.Equals(obj);
+            BitArray64 result = obj as BitArray64;
+            if (result != null)
+            {
+                return this.bits == result.bits;
+            }
+            else
+            {
+                return false;
+            }
         }
         public override int GetHashCode()
         {
-            return base.GetHashCode();
+            return this.bits.GetHashCode();
         }
         public static bool operator ==(BitArray64 a, BitArray64 b)
         {
+            return BitArray64.Equals(a, b);
         }
         public static bool operator !=(BitArray64 a, BitArray64 b)
         {
+            return !BitArray64.Equals(a, b);
         }
+        //indexer
         public int this[int index]
         {
             get
@@ -66,6 +85,17 @@ namespace Task5
                     this.bits = this.bits & (~((ulong)1 << index));
                 }
             }
+        }
+        private int[] ConvertBitsToArray()
+        {
+            int[] result = new int[64];
+            ulong temp = this.bits;
+            for (int i = 0; i < 64; i++)
+            {
+                result[i] = (int)(temp % 2);
+                temp /= 2;
+            }
+            return result;
         }
     }
 }
--- C#/OOP/CommonTypeSytem/Task5/MainClass.cs
+++ C#/OOP/CommonTypeSytem/Task5/MainClass.cs
@@ -10,7 +10,28 @@ namespace Task5
     {
         static void Main()
         {
-
+            BitArray64 num = new BitArray64(15234);
+            //test with foreach
+            foreach (var item in num)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+            //test indexer
+            for (int i = 0; i < 64; i++)
+            {
+                Console.Write(num[63-i]);
+            }
+            Console.WriteLine();
+            //compare to verify the rsult
+            Console.WriteLine( Convert.ToString(15234, 2).PadLeft(64,'0'));
+            //set any bit to zero or 1
+            num[62] = 1;
+            foreach (var item in num)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
         }
     }
 }
