e1cdf1a54f6bb35796e2523cb1e3f21c76e87a74
Dimitar Karamfilov <mpc@gbg.bg>
2013-05-05 08:59:25 +0300

    javascript advanced functions

---
+++ JavaSvript/AdvancedFunctions/javascript/MovingDivsModule.js
@@ -0,0 +1,98 @@
+﻿var movingShapes = (function () {
+    function CreateDiv(borderRadius) {//set border radius to 0 for rect or "50%" for circle
+        var currentDiv = document.createElement('div');
+        style = currentDiv.style;
+       
+        style.width = '50px';
+        style.height = '50px';
+
+        style.backgroundColor = MakeRandomColor();
+        style.color = MakeRandomColor();
+
+        style.position = "absolute";
+
+        style.borderRadius = borderRadius;
+        style.borderColor = MakeRandomColor();
+        style.borderWidth = '2px';
+        style.borderStyle = 'solid';
+
+        style.textContent = "div";
+
+        return currentDiv;
+    }
+
+    function MakeRandomColor() {
+        var red = ((Math.random() * 256) | 0);
+        var green = ((Math.random() * 256) | 0);
+        var blue = ((Math.random() * 256) | 0);
+        return "rgb(" + red + "," + green + "," + blue + ")";
+    }
+
+    function MoveInRectangle(top, left, width, height) {
+
+
+
+    }
+    function MoveInCircle(xCenter, yCenter, radius) {
+
+        var container = document.createElement('div');
+        var style = container.style;
+        var childs = [];
+
+        style.width = radius * 2 + 'px';
+        style.height = radius * 2 + 'px';
+
+        style.position = 'absolute';
+        style.borderRadius = "50%";
+
+        this.addElement = function (element) {
+            container.appendChild(element);
+            childs.push(elelement);
+            container.setAttribute("data-degees", 0);
+            return this;
+        }
+        this.moveElements = function () {
+            var elementsCount = childs.length;
+
+            for (var i = 0; i < elementsCount; i += 1) {
+                var currentElement = childs[i];
+                var angle = (parseFloat(currentElement.getAttribute('data-degrees')) + 1) % 360;
+
+                currentElement.setAttribute('data-degrees', angle);
+
+                var x = radius * Math.cos(degrees * Math.PI / 180);
+                var y = radius * Math.sin(degrees * Math.PI / 180);
+
+                currentElement.style.left = x - parseInt(currentElement.style.width, 10) / 2 + radius + 'px';
+                currentElement.style.top = y - parseInt( currentElement.style.height, 10) / 2 + radius + 'px';
+            }
+
+            this.render = function () {
+                document.body.appendChild(container);
+                return this;
+            }
+        }
+    }
+    return {
+        CreateDiv: CreateDiv,
+        Circle: MoveInCircle
+
+    }
+
+})();
+
+var circle = new movingShapes.Circle(200, 200, 100).addElement(movingShapes.CreateDiv()).render()
+
+window.requestAnimFrame = window.requestAnimationFrame ||
+   window.webkitRequestAnimationFrame ||
+   window.mozRequestAnimationFrame;
+
+(function animloop() {
+    window.requestAnimFrame(animloop);
+    render();
+})();
+
+function render() {
+    circle.moveElements();
+   // rectangle.moveElements();
+}
---
+++ JavaSvript/ObjectOrientedJavascript/VehicleModule.js
@@ -0,0 +1,182 @@
+﻿var vehicle = (function () {
+    //slide 1 point 1
+    function Vehicle(speed, propulsionUnits) {
+        this.speed = speed;
+        this.propulsionUnits = propulsionUnits;
+    }
+    Vehicle.prototype.accelerate = function () {
+        var length = this.propulsionUnits.length;
+        for (var i = 0 ; i < length; i+=1) {
+            this.speed += this.propulsionUnits[i].getAcceleration();
+        }
+    }
+    
+    //slide 1 point 2 and 3
+    function PropulsionUnit() {
+    }
+
+    PropulsionUnit.prototype.getAcceleration = function () {
+        throw new Error("not implemented finction");
+    }
+    function Wheel(radius) {
+        this.radius = radius;
+    }
+
+    Wheel.inherits(PropulsionUnit);
+
+    Wheel.prototype.getAcceleration = function () {
+        return parseInt(2 * Math.PI * this.radius);
+    }
+    
+    //slide 2 point 1
+    function PropellingNozzle(power, afterburnerState) {
+        this.power = power;
+        this.afterburnerState = afterburnerState;
+    }
+
+    PropellingNozzle.inherits(PropulsionUnit);
+
+    PropellingNozzle.prototype.getAcceleration = function () {
+        if (this.afterburnerState === AfterburnerState.ACTIVATED) {
+            return 2 * this.power;
+        }
+        else {
+            return this.power;
+        }
+    }
+
+    //slide 2 point 2
+    function Propeller(bladesNumber, rotationDirection) {
+        this.bladesNumber = bladesNumber;
+        this.rotationDirection = rotationDirection;
+    }
+
+    Propeller.inherits(PropulsionUnit);
+
+    Propeller.prototype.getAcceleration = function () {
+        if (this.rotationDirection === RotationDirection.CLOCKWISE) {
+            return this.bladesNumber;
+        }
+        else {
+            return -this.bladesNumber;
+        }
+    }
+
+    //slide 3 point 1 and 2
+    function Aircraft(speed, propellingNozzles) {
+        Vehicle.apply(this, arguments);
+    }
+    
+    Aircraft.inherits(Vehicle);
+
+    Aircraft.prototype.switchAfterburners = function (afterburnerState) {
+        for (var i = 0, len = this.propulsionUnits.length; i < len; i++) {
+            if (this.propulsionUnits[i] instanceof PropellingNozzle) {
+                this.propulsionUnits[i].afterburnerState = afterburnerState;
+            }
+        }
+    }
+
+    //slide 3 point 3
+    function Watercraft(speed, propellers) {
+        Vehicle.apply(this, arguments);
+    }
+
+    Watercraft.inherits(Vehicle);
+
+    Watercraft.prototype.setPropellersRotationDirection = function (rotationDirection) {
+        for (var i = 0; i < this.propulsionUnits.length; i++) {
+            if (this.propulsionUnits[i] instanceof Propeller) {
+                this.propulsionUnits[i].rotationDirection = rotationDirection;
+            }
+        }
+    }
+
+    //slide 4 point 1
+    function Amphibian(speed, propellers, wheels, mode) {
+        var propulsionUnits = [];
+        var propLength = propellers.length;
+
+        for (var i = 0; i < propLength; i += 1) {
+            propulsionUnits.push(propellers[i]);
+        }
+
+        var wheelsLength = wheels.length;
+        for (var j = 0; j < wheelsLength; j += 1) {
+            propulsionUnits.push(wheels[i]);
+        }
+
+        Vehicle.call(this, speed, propulsionUnits);
+        this.mode = mode;
+    }
+
+    Amphibian.inherits(Vehicle);
+    Amphibian.extends(Watercraft, "setPropellersRotationDirection");
+
+    Amphibian.prototype.accelerate = function () {
+        if (this.mode === AmphibianMode.LAND) {
+            var propUnitsLength = this.propulsionUnits.length;
+
+            for (var i = 0; i < propUnitsLength ; i += 1) {
+                if (this.propulsionUnits[i] instanceof Wheel) {
+                    this.speed += this.propulsionUnits[i].getAcceleration();
+                }
+            }
+        }
+        else {
+            var propUnitsLength = this.propulsionUnits.length;
+            
+            for (var i = 0; i < propUnitsLength; i += 1) {
+                if (this.propulsionUnits[i] instanceof Propeller) {
+                    this.speed += this.propulsionUnits[i].getAcceleration();
+                }
+            }
+        }
+    }
+    
+    //inherit + extends 
+    Function.prototype.inherits = function (parent) {
+        this.prototype = new parent();
+        this.prototype.constructor = parent;
+    };
+
+    Function.prototype.extends = function (parent) {
+        var argsLength = arguments.length;
+
+        for (var i = 1; i < argsLength; i += 1) {
+            var name = arguments[i];
+            this.prototype[name] = parent.prototype[name];
+        }
+
+        return this;
+    }
+    // enums 
+    var AfterburnerState = Object.freeze({
+        "ACTIVATED": 1,
+        "DEACTIVATED": 2
+    });
+
+    var RotationDirection = Object.freeze({
+        "CLOCKWISE": 1,
+        "COUNTERCLOCKWISE": 2
+    });
+
+    var AmphibianMode = Object.freeze({
+        "LAND": 1,
+        "WATER": 2
+    });
+    //closure functions
+    return {
+        AfterburnerState: AfterburnerState,
+        RotationDirection: RotationDirection,
+        AmphibianMode: AmphibianMode,
+        Wheel: Wheel,
+        PropellingNozzle: PropellingNozzle,
+        Propeller: Propeller,
+        LandVehicle: LandVehicle,
+        Aircraft: Aircraft,
+        Watercraft: Watercraft,
+        Amphibian: Amphibian
+    };
+}
+)();
