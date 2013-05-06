fb23322e9f7cc01b72ef2e3172e1383c0cdea8c7
Dimitar Karamfilov <mpc@gbg.bg>
2013-05-06 17:10:32 +0300

    javascript homework

---
+++ JavaSvript/EventModelHomework/Todo.js
@@ -0,0 +1,52 @@
+﻿var tasks = document.querySelector("#toDoList");
+
+function onAddTask() {
+    if (event.preventDefault) {
+        event.preventDefault();
+    }
+    else {
+        event.returnValue = false;
+    }
+
+    var currentTask = document.getElementById("taskInput").value;
+
+    var li = document.createElement("li");
+
+
+    if (currentTask !== "") {
+        li.innerHTML = currentTask;
+        tasks.appendChild(li);
+    }
+}
+
+function onRemoveTask() {
+    if (event.preventDefault) {
+        event.preventDefault();
+    }
+    else {
+        event.returnValue = false;
+    }
+
+    var ol = document.querySelector("#toDoList");
+    var elementToRemove = document.querySelectorAll("ol li");
+    ol.removeChild(elementToRemove[0]);
+    
+    
+}
+
+function onShowHide() {
+    if (event.preventDefault) {
+        event.preventDefault();
+    }
+    else {
+        event.returnValue = false;
+    }
+    var ol = document.querySelector("#toDoList");
+     
+    if (ol.style.display !== "none") {
+        ol.style.display = "none";
+    }
+    else {
+        ol.style.display = "block";
+    }
+}
---
+++ JavaSvript/EventModelHomework/carousel.js
@@ -0,0 +1,26 @@
+﻿var photos = [];
+var which = 0;
+
+photos[0] = "img/1.jpg"
+photos[1] = "img/2.jpg"
+photos[2] = "img/3.jpg"
+photos[3] = "img/4.jpg"
+photos[4] = "img/5.jpg"
+
+
+function backward() {
+    if (which > 0) {
+        window.status = ''
+        which--
+        document.images.photoslider.src = photos[which]
+    }
+}
+
+function forward() {
+    if (which < photos.length - 1) {
+        which++
+        document.images.photoslider.src = photos[which]
+    }
+    else
+        window.status = 'End of gallery'
+}
